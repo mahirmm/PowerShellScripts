49e6515d2d3a8945a1e3621fa30cd50dc52d892b
mahirmm <77577390+mahirmm@users.noreply.github.com>
2025-02-07 11:02:34 +0000

    clean up task names

--- taskCreator.ps1
+++ taskCreator.ps1
@@ -12,27 +12,25 @@ foreach ($slot in $timeSlots) { # stores each field in PS variables
     $day = $slot.Day # eg 'Monday'
     $time = $slot.Time # eg '00:00-9:00'
     $session = $slot.Session # eg 'Break'
+    $classNum = $slot.ClassNum # eg '006'
 
     # Check if the session is "Break" or empty
     if ($session -eq "Break" -or [string]::IsNullOrWhiteSpace($session)) {
         # extract the time ranges
         $startTime = $time.Split("-")[0].Trim() # start time of each break
         $endDateTime = [datetime]::ParseExact($time.Split("-")[1].Trim(), "HH:mm", $null) # endtime of each break - needs to be in datetime format for mathematical operations
-
-        # replace ":" with "-" in the task name as is illegal char in TaskScheduler task names
-        $safeStartTime = $startTime -replace ":", "-"
         $removePXEDateTime = $endDateTime.AddMinutes(-45)  # 45 min before break ends
 
         # Create scheduled task to add devices to SCCM collection and initiate reimaging
         $taskNamePrefix = "CM142Break" # prefix to add to each task's name
-        $addToCollTaskName = "$taskNamePrefix-$weekPosition-$day-$safeStartTime" # title for task name eg. 'CM142-01-Monday-00-00'
+        $addToCollTaskName = "$taskNamePrefix-$weekPosition-$day-$classNum" # title for task name eg. 'CM142-01-Monday-00-00'
         $addToCollTaskTrigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek $day -At $startTime # sets task schedule to weekly on $day variable at $startTime variable
         $addToCollTaskAction = New-ScheduledTaskAction -Execute "PowerShell" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$addToSCCMCollection`"" # command to run PS script bypassing security policies
         Register-ScheduledTask $addToCollTaskName -Action $addToCollTaskAction -Trigger $addToCollTaskTrigger -TaskPath '\CM142 Lab Breaks' # registers task in specified folder
 
         # Create scheduled task to remove devices from SCCM collection and stop new reimaging attempts
         $removePXETime = $removePXEDateTime.ToString("HH:mm") # convert date time value back to time only
-        $removePXETaskName = "$taskNamePrefix-RemoveCollection-$weekPosition-$day-$safeStartTime" # title for task name eg. 'CM142-RemoveCollection-01-Monday-00-00'
+        $removePXETaskName = "$taskNamePrefix-RemoveCollection-$weekPosition-$day-$classNum" # title for task name eg. 'CM142-RemoveCollection-01-Monday-00-00'
         $removePXETrigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek $day -At $removePXETime # sets task schedule to weekly on $day variable at $removePXETime variable
         $removePXEAction = New-ScheduledTaskAction -Execute "PowerShell" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$removeFromSCCMCollection`"" # command to run PS script bypassing security policies
         Register-ScheduledTask $removePXETaskName -Action $removePXEAction -Trigger $removePXETrigger -TaskPath "\CM142 Lab Breaks" # registers task in specified folder
