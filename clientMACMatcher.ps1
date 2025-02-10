144dda29bd4d9d61de3539e9f8b01c33e162da05
mahirmm <77577390+mahirmm@users.noreply.github.com>
2025-02-10 19:40:05 +0000

    proper imaging log

--- clientMACMatcher.ps1
+++ clientMACMatcher.ps1
@@ -2,13 +2,16 @@
 # Site configuration
 Invoke-Command -ScriptBlock {
     $logPath = "\\ODIN\Users\Administrator\Desktop\SCCM\SCCM Boot Images\Logs" # UNC path to log folder
-# Manually authenticate (replace with a valid domain user)
     $Username = "PROJECT\MAdmin"
     $Password = ConvertTo-SecureString "Shaolin1" -AsPlainText -Force
     $Credential = New-Object System.Management.Automation.PSCredential ($Username, $Password)
 
 # Map the UNC path with credentials
     New-PSDrive -Name Z -PSProvider FileSystem -Root $logPath -Credential $Credential -Persist
+
+    $adminUserSCCM = "PROJECT\Administrator"
+    $adminPassSCCM = ConvertTo-SecureString "Shaolin124?@#" -AsPlainText -Force
+    $adminCredSCCM = new-object -typename System.Management.Automation.PSCredential -argumentlist $adminUserSCCM,$adminPassSCCM
 
 # Ensure the path existsa
     if (!(Test-Path $logPath)) {
@@ -18,26 +21,10 @@ Invoke-Command -ScriptBlock {
 
     $SiteCode = "380" # Site code 
     $ProviderMachineName = "odin.project.co3808.com" # SMS Provider machine name
+    $SCCMServer = "odin.project.co3808.com"
 
     # Customizations
     $initParams = @{}
-    #$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
-    #$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
-
-    # Do not change anything below this line
-
-    # Import the ConfigurationManager.psd1 module 
-    if((Get-Module ConfigurationManager) -eq $null) {
-        Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
-    }
-
-    # Connect to the site's drive if it is not already present
-    if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
-        New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
-    }
-
-    # Set the current location to be the site code.
-    Set-Location "$($SiteCode):\" @initParams
 
     # obtain MAC of device NIC
     $MAC = (Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object { $_.MACAddress -and $_.IPEnabled }).MACAddress
@@ -54,15 +41,36 @@ Invoke-Command -ScriptBlock {
         $computerName = "Unknown-PC" # no matching MAC found
     }
 
+    $collectionSearcher = Get-WmiObject -credential $adminCredSCCM -ComputerName $SCCMServer -Namespace "root/SMS/site_$SiteCode" `
+        -Query "SELECT SMS_Collection.* FROM SMS_FullCollectionMembership, SMS_Collection WHERE name = '$computerName' AND SMS_FullCollectionMembership.CollectionID = SMS_Collection.CollectionID AND CollectionID NOT LIKE 'SMS%'"
+    write-host $collectionSearcher
+
+    if ($computerName -eq "Unknown-PC"){
+            break
+    } else {
+        # REF
+        ## https://sccmentor.com/2015/04/16/find-collection-membership-for-a-device-with-powershell/
+        $collectionSearcher = Get-WmiObject -credential $adminCredSCCM -ComputerName $SCCMServer -Namespace root/SMS/site_$SiteCode -Query "SELECT SMS_Collection.* FROM SMS_FullCollectionMembership, SMS_Collection where name = '$computerName' and SMS_FullCollectionMembership.CollectionID = SMS_Collection.CollectionID and CollectionID NOT LIKE 'SMS%'"
+        $collectionSearcher = $collectionSearcher | Select-Object -Property Name
+        $collectionList = "" # empty string to hold 'list'
+        if ($collectionSearcher){ # if values found
+            foreach ($collection in $collectionSearcher) { # for every collection in the list
+                $collectionList += $collection.Name + ", " # append collection name to string # eg. 'name1, name2, name3'
+            }
+        } else { # if no values found
+            $collectionList = "No collections"
+        }
+    }
+
+
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss" # current date/time
     $logFile = "$logPath\$computerName-Imaging.log" # file name to create/append
-    #$logFile = "$logPath\TS-Imaging.log" # file name to create/append
 
     # append data to log file
     "--------------------------------------------------" | Out-File -FilePath ('FileSystem::' + $logFile) -Append # append data to file / create file if not exist
     "Imaging Start: $timestamp" | Out-File -FilePath ('FileSystem::' + $logFile) -Append
-    #"Included Collections: $collectionList" | Out-File -FilePath ('FileSystem::' + $logFile) -Append
     "Computer Name: $computerName" | Out-File -FilePath ('FileSystem::' + $logFile) -Append
-    #"MACreal: $MACreal" | Out-File -FilePath ('FileSystem::' + $logFile) -Append
-    #"--------------------------------------------------" | Out-File -FilePath ('FileSystem::' + $logFile) -Append
+    "Included Collections: $collectionList" | Out-File -FilePath ('FileSystem::' + $logFile) -Append
+    # "Imaging End: $timestamp" | Out-File -FilePath ('FileSystem::' + $logFile) -Append
+    # "--------------------------------------------------" | Out-File -FilePath ('FileSystem::' + $logFile) -Append
 }
---
+++ createSecurePass.ps1
@@ -0,0 +1,3 @@
+﻿#securepass.txt file
+read-host -prompt "Enter password to be encrypted in securepass.txt "
+-assecurestring | convertfrom-securestring | out-file "\\ODIN\Users\Administrator\Desktop\SCCM\SCCM Boot Images\PowerShellScripts\adminPass.txt"
